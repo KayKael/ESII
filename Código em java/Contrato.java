86f812f963a204e8cfc0b526c33dc8e14bd68f67
Kael <41486985+KayKael@users.noreply.github.com>
2023-12-28 20:08:55 +0000

    Add files via upload

---
+++ Código em java/Contrato.java
@@ -0,0 +1,75 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Contrato {
+
+    /**
+     * Default constructor
+     */
+    public Contrato() {
+    }
+
+    /**
+     * 
+     */
+    public int ID_contrato;
+
+    /**
+     * 
+     */
+    public double valor_do_contrato;
+
+    /**
+     * 
+     */
+    public float inflação;
+
+    /**
+     * 
+     */
+    public string tipo_contrato;
+
+    /**
+     * 
+     */
+    public double orçamento;
+
+    /**
+     * 
+     */
+    public int ano;
+
+    /**
+     * @return
+     */
+    public double calcular_valor_contrato() {
+        // TODO implement here
+        return 0.0d;
+    }
+
+    /**
+     * 
+     */
+    public void getters() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void setters() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public double calcular_orçamento_previsto() {
+        // TODO implement here
+        return 0.0d;
+    }
+
+}
---
+++ Código em java/Valores Propostos.java
@@ -0,0 +1,75 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Valores Propostos {
+
+    /**
+     * Default constructor
+     */
+    public Valores Propostos() {
+    }
+
+    /**
+     * 
+     */
+    public int id_proposto;
+
+    /**
+     * 
+     */
+    public int serviço;
+
+    /**
+     * 
+     */
+    public double valor_proposto;
+
+    /**
+     * 
+     */
+    public float percentagem_proposta;
+
+    /**
+     * 
+     */
+    public float percentagem_exec;
+
+    /**
+     * 
+     */
+    public double execução_material;
+
+    /**
+     * @return
+     */
+    public double calcular_eficiência_distribuição() {
+        // TODO implement here
+        return 0.0d;
+    }
+
+    /**
+     * @return
+     */
+    public double calcular_valor_proposto() {
+        // TODO implement here
+        return 0.0d;
+    }
+
+    /**
+     * 
+     */
+    public void Getters() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Setters() {
+        // TODO implement here
+    }
+
+}
