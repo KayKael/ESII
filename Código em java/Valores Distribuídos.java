21debae9b3f982b182110e3b4d696db413d1799e
de Sá <kael.desa@keyloop.com>
2024-02-05 22:16:27 +0000

    update

---
+++ Código em java/Valores Distribuídos.java
@@ -0,0 +1,54 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Valores Distribuídos {
+
+    /**
+     * Default constructor
+     */
+    public Valores Distribuídos() {
+    }
+
+    /**
+     * 
+     */
+    public int id_distruobuicao;
+
+    /**
+     * 
+     */
+    public int serviço;
+
+    /**
+     * 
+     */
+    public double valor_inicial;
+
+    /**
+     * 
+     */
+    public double execução_material;
+
+    /**
+     * 
+     */
+    public float porcentagem_exec;
+
+    /**
+     * 
+     */
+    public void getters() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void setters() {
+        // TODO implement here
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# ESII
+
+## Trabalho de Eficiência da distribuição do orçamento de contratos de bens dos Tribunais de Portugal
+
+
+//[UseCase](https://github.com/KayKael/ESII/blob/kael/ESII/Use%20case%20cliente%20tribunal.mdj)
